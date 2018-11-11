4c0ddc1057193a12a9d0e8eac0623838dfc0b874
Felix König <felix.koenig@protonmail.com>
2018-11-11 14:54:51 +0100

    Depth image included

--- tuw_door_detection/src/imgproc/door_detector_imgproc_node.cpp
+++ tuw_door_detection/src/imgproc/door_detector_imgproc_node.cpp
@@ -12,9 +12,10 @@ DoorDetectorImageProcessorNode::ParametersNode::ParametersNode() {
 }
 
 DoorDetectorImageProcessorNode::DoorDetectorImageProcessorNode() : nh_("") {
-    sub_image_ = nh_.subscribe("image_rgb", 1000, &DoorDetectorImageProcessorNode::callbackImage, this);
-    //sub_image_depth_ = nh_.subscribe("depth_image", 1000, &DoorDetectorImageProcessorNode::callbackDepthImage, this);
-    //sub_image_depth_ = nh_.subscribe("depth", 1000, &DoorDetectorImageProcessorNode::callbackImage, this);
+  sub_image_ = nh_.subscribe("image_rgb", 1000, &DoorDetectorImageProcessorNode::callbackImage, this);
+  sub_image_depth_ = nh_.subscribe("image_depth", 1000, &DoorDetectorImageProcessorNode::callbackDepthImage, this);
+  //sub_image_depth_ = nh_.subscribe("depth_image", 1000, &DoorDetectorImageProcessorNode::callbackDepthImage, this);
+  //sub_image_depth_ = nh_.subscribe("depth", 1000, &DoorDetectorImageProcessorNode::callbackImage, this);
 }
 
 DoorDetectorImageProcessorNode::~DoorDetectorImageProcessorNode() {
@@ -22,39 +23,38 @@ DoorDetectorImageProcessorNode::~DoorDetectorImageProcessorNode() {
 }
 
 void DoorDetectorImageProcessorNode::callbackImage(const sensor_msgs::ImageConstPtr &_img) {
-    auto img = cv_bridge::toCvCopy(_img, std::string("CV_8UC3"));
-    image_rgb_ = img;
-    std::cout << "image gotten" << std::endl;
+  image_rgb_ = cv_bridge::toCvCopy(_img, std::string("8UC3"));
 }
 
 void DoorDetectorImageProcessorNode::callbackDepthImage(const sensor_msgs::ImageConstPtr &_img) {
-    auto img = cv_bridge::toCvCopy(_img, std::string("CV_8UC3"));
-    image_depth_ = img;
+  image_depth_ = cv_bridge::toCvCopy(_img, std::string("16UC1"));
+  image_depth_->image.convertTo(image_depth_->image, CV_32FC1,
+                                1.0 / static_cast<double>(std::numeric_limits<u_int16_t>::max()));
+
 }
 
 void DoorDetectorImageProcessorNode::display() {
-    std::cout << "display" << std::endl;
-    if (image_rgb_)
-    {
-        cv::namedWindow("rgb image");
-        cv::imshow("rgb image", image_rgb_->image);
-        cv::waitKey(1);
-    }
+  if (image_rgb_) {
+    cv::namedWindow("rgb image");
+    cv::imshow("rgb image", image_rgb_->image);
+    cv::waitKey(1);
+  }
 
-    if (image_depth_)
-    {
-        cv::namedWindow("depth image");
-        cv::imshow("depth image", image_depth_->image);
-        cv::waitKey(1);
-    }
+  if (image_depth_) {
+    cv::namedWindow("depth image");
+    cv::imshow("depth image", image_depth_->image);
+    cv::waitKey(1);
+  }
 }
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "door_detector_image_processor_node");
-    DoorDetectorImageProcessorNode node;
+  ros::init(argc, argv, "door_detector_image_processor_node");
+  DoorDetectorImageProcessorNode node;
+  ros::Rate rate(20);
 
-    while (ros::ok()) {
-        ros::spinOnce();
-        node.display();
-    }
+  while (ros::ok()) {
+    ros::spinOnce();
+    node.display();
+    rate.sleep();
+  }
 }
