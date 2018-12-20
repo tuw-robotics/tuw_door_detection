18db72801ba38317bc4da20aa58fc118fdad9b00
Felix König <felix0koenig@gmail.com>
2018-12-20 15:17:34 +0100

    Dynamic Reconfigure

--- tuw_door_detection/include/laserproc/door_depth_detector.h
+++ tuw_door_detection/include/laserproc/door_depth_detector.h
@@ -3,12 +3,14 @@
 
 #include "door_detector_base.h"
 #include "door_detection.h"
+
 #include <ros/publisher.h>
 #include <geometry_msgs/PoseWithCovariance.h>
 #include <tuw_geometry/figure.h>
-#include <tuw_door_detection/DepthDetectorConfig.h>
 #include <tuw_measurement_utils/contour.h>
+
 #include <dynamic_reconfigure/server.h>
+#include <tuw_door_detection/DepthDetectorConfig.h>
 
 namespace tuw
 {
@@ -16,12 +18,12 @@ namespace tuw
   {
     class DoorDepthDetector : public DoorDetectorBase
     {
-
+    
     public:
       struct ParametersNode
       {
         ParametersNode();
-
+        
         ros::NodeHandle node;
         bool debug;
         std::string world_frame;
@@ -29,7 +31,7 @@ namespace tuw
         std::string publisher_topic;
         std::string internal_mode;
       };
-
+      
       //TODO: use callback from parameterserver
       struct Config
       {
@@ -45,7 +47,7 @@ namespace tuw
           map_grid_y = 1;
           map_rotation = 0;
         }
-
+        
         double map_pix_x;
         double map_pix_y;
         double map_max_x;
@@ -56,51 +58,53 @@ namespace tuw
         double map_grid_y;
         double map_rotation;
       };
-
+      
       std::unique_ptr<ParametersNode> params_;
       std::unique_ptr<Config> config_;
-
+      
       ///// parameter server for dynamic detector configuration
       // dynamic_reconfigure::Server<tuw_door_detection::DepthDetectorConfig> reconfigure_server_;
-
+      
       // /// parameter server callback
       // dynamic_reconfigure::Server<tuw_door_detection::DepthDetectorConfig>::CallbackType reconfigure_fnc_;
-
+      
       ///**
       //  * @brief callback function on incoming parameter changes
       //  * @param config the configuration message
       //  * @param level not used here, but required for dynamic reconfigure callbacks
       //  */
       // void callbackConfig(tuw_door_detection::DepthDetectorConfig &_config, uint32_t level);
-
-      DoorDepthDetector(ros::NodeHandle &_nh);
-
+      
+      DoorDepthDetector( ros::NodeHandle &_nh );
+      
       virtual ~DoorDepthDetector();
-
+      
       WorldScopedMaps &getMap()
       {
         return ws_map_;
       }
-
+      
       std::vector<std::shared_ptr<tuw::Contour>> getContours()
       {
         return contours_;
       }
-
+      
+      void reconfigureCallback( tuw_door_detection::DepthDetectorConfig &_config, uint32_t level );
+    
     protected:
       //std::unique_ptr<tuw_door_detection::DepthDetectorConfig> config_;
-
-      bool processLaser(const sensor_msgs::LaserScan &_laser) override;
-
-      void plot(const std::vector<double> &_responses);
-
-      void plot(const std::vector<DoorDetectionPtr> &_detections);
-
+      
+      bool processLaser( const sensor_msgs::LaserScan &_laser ) override;
+      
+      void plot( const std::vector<double> &_responses );
+      
+      void plot( const std::vector<DoorDetectionPtr> &_detections );
+      
       template<typename T>
-      std::vector<T> normalize(std::vector<T> &_resp);
-
+      std::vector<T> normalize( std::vector<T> &_resp );
+      
       WorldScopedMaps ws_map_;
-
+    
     private:
       Figure figure_local_;
       float thresh_{0.3};
@@ -108,17 +112,23 @@ namespace tuw
       ros::Publisher pubObjectDetections_;
       std::map<size_t, cv::Scalar> colorMap_;
       std::vector<std::shared_ptr<tuw::Contour>> contours_;
-
+      tuw_door_detection::DepthDetectorConfig reconfigure_config_;
+      
+      //reconfigure stuff
+      dynamic_reconfigure::Server<tuw_door_detection::DepthDetectorConfig>::CallbackType cb_type_;
+      dynamic_reconfigure::Server<tuw_door_detection::DepthDetectorConfig> cb_server_;
+      
       std::unique_ptr<ParametersNode> &params()
       {
         return params_;
       }
-
-      bool structureMode(const sensor_msgs::LaserScan &_laser, std::vector<DoorDetectionPtr> &_detections);
-
-      bool kernelMode(const sensor_msgs::LaserScan &_laser, std::vector<DoorDetectionPtr> &_detections);
-
-      std::vector<std::shared_ptr<tuw::Contour>> contourMode(const sensor_msgs::LaserScan &_laser);
+      
+      bool structureMode( const sensor_msgs::LaserScan &_laser, std::vector<DoorDetectionPtr> &_detections );
+      
+      bool kernelMode( const sensor_msgs::LaserScan &_laser, std::vector<DoorDetectionPtr> &_detections );
+      
+      std::vector<std::shared_ptr<tuw::Contour>> contourMode( const sensor_msgs::LaserScan &_laser );
+      
     };
   }
 }
