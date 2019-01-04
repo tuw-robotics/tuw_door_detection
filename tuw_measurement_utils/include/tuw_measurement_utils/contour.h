e8d1e7e2d84e218fdc636f89a346d4268c164a0c
Felix König <felix.koenig@protonmail.com>
2019-01-04 16:44:03 +0100

    Missing Eigen/Core

--- tuw_measurement_utils/include/tuw_measurement_utils/contour.h
+++ tuw_measurement_utils/include/tuw_measurement_utils/contour.h
@@ -9,15 +9,16 @@
 #include <memory>
 #include <tuw_geometry/world_scoped_maps.h>
 #include <tuw_geometry/linesegment2d_detector.h>
+#include <Eigen/Core>
 
 namespace tuw
 {
-  
+
   class Contour
   {
-  
+
   public:
-    
+
     //class CandidateAttributes
     //{
     //public:
@@ -38,203 +39,203 @@ namespace tuw
     //  bool crossing_segs_;
     //  bool door_candidate_;
     //};
-    
+
     class Beam
     {
     public:
       Beam()
       {
       };
-      
-      Beam( double range, double angle, Point2D end_point );
-      
+
+      Beam(double range, double angle, Point2D end_point);
+
       //Beam(const Beam &) = delete;
-      
+
       //Beam &operator=(const Beam &) = delete;
-      
+
       ~Beam() = default;
-      
+
       Point2D end_point;
       Point2D img_coords;
       double range;
       double angle;
-      
+
       const bool is_valid() const;
-      
-      void set_valid( const bool v );
-      
+
+      void set_valid(const bool v);
+
       const bool get_is_visible() const;
-      
-      void set_is_visible( const bool v );
-      
-      static std::shared_ptr<Beam> make_beam( double range, double angle, Point2D end_point );
-    
+
+      void set_is_visible(const bool v);
+
+      static std::shared_ptr<Beam> make_beam(double range, double angle, Point2D end_point);
+
     private:
       bool valid_beam;
       bool is_visible_;
     };
-    
+
     struct CVDefect
     {
-      CVDefect( int startIdx, int endIdx, int x, int y )
+      CVDefect(int startIdx, int endIdx, int x, int y)
       {
         start_idx = startIdx;
         end_idx = endIdx;
-        point = cv::Vec2i( x, y );
+        point = cv::Vec2i(x, y);
       }
-      
+
       cv::Vec2i point;
       int start_idx;
       int end_idx;
     };
-    
+
     struct Corner
     {
-      
-      Corner( const cv::Point2d &_point, size_t _response, size_t _idx )
+
+      Corner(const cv::Point2d &_point, size_t _response, size_t _idx)
       {
         point = _point;
         response = _response;
         idx = _idx;
       }
-      
+
       cv::Point2d point;
       double response;
       size_t idx;
     };
-    
+
     Contour();
-    
-    void push_back( std::shared_ptr<Beam> beam );
-    
-    void detectCorners( const size_t KERNEL_SIZE );
-    
-    void cvConvexityDefects( tuw::WorldScopedMaps &_map );
-    
+
+    void push_back(std::shared_ptr<Beam> beam);
+
+    void detectCorners(const size_t KERNEL_SIZE);
+
+    void cvConvexityDefects(tuw::WorldScopedMaps &_map);
+
     void cvDetectCorners();
-    
-    void detectLines( LineSegment2DDetector &lineSegment2DDetector );
-    
-    void registerToImage( const Eigen::Matrix4d &tf,
-                          double fx, double fy,
-                          double cx, double cy,
-                          double tx, double ty );
-    
-    void visibilityCheck( bool shift_lines, int img_width, int img_height );
-    
+
+    void detectLines(LineSegment2DDetector &lineSegment2DDetector);
+
+    void registerToImage(const Eigen::Matrix4d &tf,
+                         double fx, double fy,
+                         double cx, double cy,
+                         double tx, double ty);
+
+    void visibilityCheck(bool shift_lines, int img_width, int img_height);
+
     const std::vector<std::unique_ptr<Corner>> &getCorners();
-    
+
     const std::vector<LineSegment2DDetector::LineSegment> &getLineSegments()
     {
       return line_segments_;
     };
-    
+
     const std::vector<std::pair<Point2D, Point2D>> &getLineSegmentImageCoords()
     {
       return line_segment_img_coords_;
     }
-    
-    void renderInternal( tuw::WorldScopedMaps &map );
-    
+
+    void renderInternal(tuw::WorldScopedMaps &map);
+
     bool is_door_candidate()
     {
       return is_door_candidate_;
     }
-    
-    void set_door_candidate( bool val )
+
+    void set_door_candidate(bool val)
     {
       this->is_door_candidate_ = val;
     }
-    
+
     std::shared_ptr<Beam> front()
     {
       beams_.front();
     }
-    
+
     std::shared_ptr<Beam> back()
     {
       beams_.back();
     }
-    
+
     std::vector<std::shared_ptr<Beam>> &beams()
     {
       return beams_;
     }
-    
+
     const std::vector<std::shared_ptr<Beam>> &beams() const
     {
       return beams_;
     }
-    
-    void render( WorldScopedMaps &map2image, cv::Mat &image, cv::Scalar &color, double rad = 2, bool corners = true );
-    
+
+    void render(WorldScopedMaps &map2image, cv::Mat &image, cv::Scalar &color, double rad = 2, bool corners = true);
+
     const Point2D &startPoint() const
     {
       const auto elem = *beams_.begin();
-      assert ( elem );
+      assert (elem);
       return elem->end_point;
     }
-    
+
     const Point2D &endPoint() const
     {
       const auto elem = *beams_.end();
-      assert ( elem );
+      assert (elem);
       return elem->end_point;
     }
-    
+
     Point2D &startPoint()
     {
       const auto elem = *beams_.begin();
-      assert ( elem );
+      assert (elem);
       return elem->end_point;
     }
-    
+
     Point2D &endPoint()
     {
       const auto elem = *beams_.end();
-      assert ( elem );
+      assert (elem);
       return elem->end_point;
     }
-    
+
     std::vector<std::shared_ptr<Beam>>::const_iterator begin() const
     {
       return beams_.begin();
     }
-    
+
     std::vector<std::shared_ptr<Beam>>::iterator begin()
     {
       return beams_.begin();
     }
-    
+
     std::vector<std::shared_ptr<Beam>>::const_iterator end() const
     {
       return beams_.end();
     }
-    
+
     std::vector<std::shared_ptr<Beam>>::iterator end()
     {
       return beams_.end();
     }
-    
+
     const std::vector<std::shared_ptr<Contour>> &getChildren() const
     {
       return children_;
     }
-    
-    void addChild( std::shared_ptr<Contour> &child )
+
+    void addChild(std::shared_ptr<Contour> &child)
     {
-      children_.push_back( child );
+      children_.push_back(child);
     }
-    
+
     const cv::Scalar &getAssignedColor() const
     {
       return assigned_color_;
     }
-    
+
     double length();
-    
-    bool optimizeLines( const unsigned int iterations );
-  
+
+    bool optimizeLines(const unsigned int iterations);
+
   private:
     std::vector<std::shared_ptr<Beam>> beams_;
     std::vector<std::unique_ptr<Corner>> corner_points_;
@@ -246,10 +247,10 @@ namespace tuw
     size_t num_corners_;
     cv::Mat rendering_;
     bool is_door_candidate_;
-    
+
     cv::Scalar assigned_color_;
     cv::Scalar candidate_color_;
-    
+
     std::vector<std::unique_ptr<CVDefect>> convexity_defects_;
   };
 };
