a77f9ba4af1a3a4ce2dc96df0930cc5417105a51
Felix König <felix0koenig@gmail.com>
2019-04-26 14:29:18 +0200

    WIP visual improvements of function plots

--- sensor_model_evaluator/python/plots_for_paper.py
+++ sensor_model_evaluator/python/plots_for_paper.py
@@ -14,11 +14,13 @@ def decay(x_range, z_star):
     return y_exp
 
 
-def lambda_decay(x_range, lambda_short, z_star, z_maxval=50):
-    #return (1.0 / (1.0 - np.exp(-lambda_short * z_star))) * lambda_short * np.exp(-lambda_short * x_range)
-    return (1.0 / (1.0 - np.exp(-lambda_short * z_star))) * lambda_short * np.exp(-lambda_short * x_range)
+def pshort(x_range, lambda_short, z_star, stepsize):
+    result = (1.0 / (1.0 - np.exp(-lambda_short * z_star))) * lambda_short * np.exp(-lambda_short * x_range)
+    result[int((1.0 / stepsize) * z_star):] = 0
+    return result
 
-def uniform_noise(x_range, z_max_val, offset):
+
+def prand(x_range, z_max_val, offset):
     y_uni = np.zeros(x_range.shape)
     y_uni[:] = (1.0 / z_max_val)
     y_uni[:offset] = 0
@@ -26,42 +28,47 @@ def uniform_noise(x_range, z_max_val, offset):
     return y_uni
 
 
-def z_max_fun(x_range, z_max_val, offset):
+def pmax(x_range, stepsize, z_max, offset):
     y_uni = np.zeros(x_range.shape)
-    y_uni[(z_max_val - offset):(z_max_val + offset)] = 0.025
+    px_max = int((1.0 / stepsize) * z_max)
+    y_uni[(px_max - offset):(px_max + offset)] = 0.025
     return y_uni
 
 
 max_val = 50
-z_max_val = 45
-offset = 24
-z_star = 200
+offset = 7
+z_star = 25
+lambda_short = 0.9
 
-x_range = np.arange(0, max_val, 1)
+stepsize = 0.1
+x_range = np.arange(0, max_val, stepsize)
 y_normpdf = mlab.normpdf(x_range, z_star, 0.5)
 
-plt.subplot(2, 2, 1)
-plt.plot(x_range, y_normpdf)
-plt.xlabel("range")
-plt.ylabel("p_hit")
-plt.title("Gaussian")
+fig, (axes0, axes1) = plt.subplots(1, 2, gridspec_kw = {'width_ratios':[1, 1], 'height_ratios':[0.20]})
+axes0.plot(x_range, y_normpdf)
+axes0.set_xlabel("range")
+axes0.set_ylabel("p_hit")
+axes0.set_title("Gaussian")
 
-plt.subplot(2, 2, 2)
-plt.plot(x_range, lambda_decay(x_range, 0.05, z_star))
-plt.xlabel("range")
-plt.ylabel("p_short")
-plt.title("Occlusions")
+axes1.plot(x_range, pshort(x_range, 0.1, z_star, stepsize))
+axes1.set_xlabel("range")
+axes1.set_ylabel("p_short")
+axes1.set_title("Occlusions")
 
-plt.subplot(2, 2, 3)
-plt.plot(x_range, z_max_fun(x_range, z_max_val, offset))
+plt.show(block=True)
+plt.figure()
+
+plt.subplot(1, 2, 1)
+plt.plot(x_range, pmax(x_range, stepsize, max_val - 5, offset))
+plt.axes[0, 0]
 plt.xlabel("range")
 plt.ylabel("p_max")
 plt.title("Max readings")
 
 x_range_append = np.concatenate([np.arange(-20, 0, 1), x_range, np.arange(len(x_range) - 1, len(x_range) + 20, 1)],
                                 axis=0)
-plt.subplot(2, 2, 4)
-plt.plot(x_range_append, uniform_noise(x_range_append, z_max_val, 20))
+plt.subplot(1, 2, 2)
+plt.plot(x_range_append, prand(x_range_append, stepsize, 20))
 plt.xlabel("range")
 plt.ylabel("p_rand")
 plt.title("Uniform noise")
